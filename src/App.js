02df0f20e5d98847bf250d73daeaad63e1f20e02
TechGeekNeXt <techgeeknext@gmail.com>
2021-10-26 19:33:08 +0530

    tgn

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UploadFile from './components/UploadFile';
+
+function App() {
+    
+  return (
+    <div className="App">
+     <h2>TechGeekNext Multiple File Upload Example</h2>
+    <UploadFile />    
+    </div>
+  );
+}
+
+export default App;
